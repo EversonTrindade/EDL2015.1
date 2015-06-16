b1ed8cf7f8cfb991fc1a80cce94b27f2a0cff2a1
EversonTrindade <eversonpereira@ymail.com>
2015-06-16 00:39:30 -0300

    Deque

--- EDL2015.1/src/tads/ed/C/deque/DequeException.java
+++ EDL2015.1/src/tads/ed/C/deque/DequeException.java
@@ -1,5 +1,13 @@
 package tads.ed.C.deque;
 
 public class DequeException extends Exception {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
+	public DequeException(String err){
+		super(err);
+	}
 }
---
+++ EDL2015.1/src/tads/ed/C/deque/DequeExec.java
@@ -0,0 +1,9 @@
+package tads.ed.C.deque;
+
+public class DequeExec {
+
+	public static void main(String[] args) {
+
+	}
+
+}
