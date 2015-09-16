32ad8c5394315f8c408ee72591999aa93af111d9
EversonTrindade <eversonpereira@ymail.com>
2015-09-16 16:49:58 -0300

    implm

--- EDL2015.1/src/tads/ed/D/listSimpEncad/ListaLigadaExec.java
+++ EDL2015.1/src/tads/ed/D/listSimpEncad/ListaLigadaExec.java
@@ -5,15 +5,17 @@ public class ListaLigadaExec {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ListaLigadaImpl lista = new ListaLigadaImpl();
-		lista.insertElem(2);
-		lista.insertElem(22);
-		lista.insertElem(222);
-		lista.showElement();
-		lista.removeElem();
-		lista.showElement();
-		System.out.println("Get Size:" + lista.getSize());
-		lista.insertElemAfter(3333);
-		lista.showElement();
+		lista.insertElemFirst(1);
+		lista.insertElemFirst(2);
+		lista.insertElemLast(3);
+		lista.insertElemFirst(4);
+		lista.insertElemLast(5);
+				
+		System.out.println("Leaf: " +lista.getLeaf().getElement());
+		System.out.println("Root: " +lista.getRoot().getElement());
+		
+		System.out.println("Lista de tamanho: " + lista.getSize());
+
 	}
 
 }
--- EDL2015.1/src/tads/ed/D/listSimpEncad/ListaLigadaImpl.java
+++ EDL2015.1/src/tads/ed/D/listSimpEncad/ListaLigadaImpl.java
@@ -3,77 +3,66 @@ package tads.ed.D.listSimpEncad;
 public class ListaLigadaImpl {
 
 	private No root;
-	//private No leaf;
-	private int size = 1;
+	private No leaf;
+	private int size = 0;
 
-	public void insertElem(Object elem) {
-		if (this.root == null) {
-			this.root = new No(elem);
+	public void insertElemFirst(Object elem) {
+		No newNode = new No(elem);
+		leaf = newNode;
+		if (root == null) {
+			root = leaf;
 			root.setNext(null);
+		}
+		size = size + 1;		
+	}
+
+	public void insertElemLast(Object elem){
+		if (root == null) {
+			insertElemFirst(elem);
 		} else {
 			No newNode = new No(elem);
-			newNode.setNext(root);
+			root.setNext(newNode);
 			root = newNode;
 			size = size + 1;
-			
-		}
-	}
-	//Incompleto - falta o fim da lista apontar para o novo n—
-	//com a ajuda da leaf
-	public void insertElemAfter(Object elem){
-		if (this.root == null) {
-			this.root = new No(elem);
-			root.setNext(null);
-		} else {
-			No newNode = new No(elem);
-			newNode.setNext(null);
-			No temp = null;
-			while(this.root != null){
-				if(this.root.getNext() == null)
-					temp = this.root.getNext();
-				this.root = this.root.getNext();
-			}
-			temp.setNext(newNode);
 		}
 	}
 	
-	public Object removeElem() {
-		if(root == null){
-			throw new IllegalArgumentException("Lista vazia");
-		}else{
-			No temp;
-			temp = root.getNext();
-			root = null;
-			root = temp;
-			size = size - 1;
-				
-		}
-		
+//	public Object removeElemFirst() {
+//		if(root == null){
+//			throw new IllegalArgumentException("Lista vazia");
+//		}else{
+//			No temp = leaf;
+//			leaf.setNext(temp);
+//			leaf = null;
+//			size = size - 1;
+//		}
+//		return null;
+//	}
+	
+	public Object removeElemLast(){
 		return null;
 	}
-	
-	public void showElement(){
-		System.out.println("Show Elements");
-		No temp = root;
-		while(temp != null){
-			System.out.println("Valor: " +temp.getElement());
-			if(temp.getNext() != null){
-				temp.getNext();
-			}
-			temp = temp.getNext();
-		}	
+
+	public void setRoot(No root) {
+		this.root = root;
 	}
 	
 	public No getRoot() {
 		return root;
 	}
 
-	public void setRoot(No root) {
-		this.root = root;
+	public void setSize(int size) {
+		this.size = size;
 	}
 
 	public int getSize() {
 		return size;
+	}
+	public No getLeaf() {
+		return leaf;
+	}
+	public void setLeaf(No leaf) {
+		this.leaf = leaf;
 	}
 
 	
